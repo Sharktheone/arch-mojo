5aa64cd71be5307ddb0cb14549f95786b8303e4e
Sharktheone <sharktheone@proton.me>
2023-09-17 16:01:07 +0200

    use ~/.local/lib for libraries

--- install.py
+++ install.py
@@ -13,7 +13,7 @@ arch = "x86_64-linux-gnu"
 
 questions = [
     inquirer.Confirm("modular", message="Do you already have modular installed?"),
-    inquirer.Confirm("persistent", message="Do you want to install the libraries persistent?"),
+    inquirer.Confirm("persistent", message="Do you want to install the libraries globally (for all users)?"),
     inquirer.Confirm("venv", message="Do you want to automatically use a venv when running modular install/update?"),
     inquirer.Path("path", message="Where do you want to create the venv and temporary files. (the venv is needed for "
                                   "installation anyways) Press enter to use a tmp dir (not when using the venv)",
@@ -21,7 +21,6 @@ questions = [
     inquirer.Password("token", message="Please enter your Modular auth token. You can also type 'manual' to run "
                                        "modular manually when requested"),
 ]
-
 
 answers = inquirer.prompt(questions)
 
@@ -59,15 +58,14 @@ urllib.request.urlretrieve("http://ftp.debian.org/debian/pool/main/n/ncurses/lib
 os.system(f"cd {WORKING_DIR} && ar -vx libncurses.deb && tar -xf data.tar.xz")
 
 # copy libs
-
-if answers["persistent"]:
+if answers["global"]:
     os.system(f"sudo cp {WORKING_DIR}lib/{arch}/* /lib/")
     os.system(f"sudo cp {WORKING_DIR}usr/lib/{arch}/* /usr/lib/")
     os.system(f"sudo cp {WORKING_DIR}lib/{arch}/* /usr/lib/")
 
 
 else:
-    mojo_lib_path = "/home/$USER/.modular/pkg/packages.modular.com_mojo/lib"
+    mojo_lib_path = "/home/$USER/.local/lib/mojo"
 
     os.system(f"mkdir -p {mojo_lib_path}")
 
@@ -81,6 +79,23 @@ os.system(f"python3 -m venv {WORKING_DIR}venv")
 os.system(f"source {WORKING_DIR}venv/bin/activate")
 os.system("modular install mojo")
 
+rc_path = ""
+
+match os.environ["SHELL"].split("/")[-1]:
+    case "bash":
+        rc_path.join("~/.bashrc")
+    case "zsh":
+        rc_path.join("~/.zshrc")
+    case _:
+        shell_questions = [
+            inquirer.Text("rc-path",
+                          message="Please enter the path to your shell rc file (e.g. ~/.bashrc for bash)"),
+        ]
+        shell_answers = inquirer.prompt(shell_questions)
+        rc_path.join(shell_answers["rc-path"])
+
+rc_file = open(rc_path, "a")
+
 if answers["venv"]:
     os.system(f"python3 -m venv {WORKING_DIR}venv")
     urllib.request.urlretrieve("https://raw.githubusercontent.com/Sharktheone/arch-mojo/main/shell.sh",
@@ -90,23 +105,17 @@ if answers["venv"]:
 
     shell.replace("{{venv-path}}", f"{WORKING_DIR}venv")
 
-    rc_path = ""
-
-    match os.environ["SHELL"].split("/")[-1]:
-        case "bash":
-            rc_path.join("~/.bashrc")
-        case "zsh":
-            rc_path.join("~/.zshrc")
-        case _:
-            shell_questions = [
-                inquirer.Text("rc-path",
-                              message="Please enter the path to your shell rc file (e.g. ~/.bashrc for bash)"),
-            ]
-            shell_answers = inquirer.prompt(shell_questions)
-            rc_path.join(shell_answers["rc-path"])
-
-    rc_file = open(rc_path, "a")
     rc_file.write(shell)
+
+exports = \
+    """
+export PATH=$PATH:~/.modular/pkg/packages.modular.com_mojo/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib/mojo
+"""
+
+rc_file.write(exports)
+
+rc_file.close()
 
 # delete temp files
 # maybe here a check would make sense if the WORKING_DIR is already existing
